2e4cb293fdb7787112017dc20bdc6564042ad61d
Aleksey Petrov <mail@petroffs.com>
2021-12-21 21:22:54 +0300

    autoloader fx

--- autoloader.php
+++ autoloader.php
@@ -6,16 +6,6 @@ function autoLoader($name)
 		$name = str_replace('\\', '/', $name);
 		$name = substr($name, 0, strrpos($name, "/")) . '.php';
 		require_once __DIR__ . '/packages/' . $name;
-	} elseif (strstr($name, '123WeppsExtensions\\')) {
-		//     	$name = str_replace('\\', '/', $name);
-		//     	$name = str_replace('/Extension', '/', $name);
-		//     	$name = substr($name, 0, strrpos($name, "PPS")) . '.php';
-		//         require_once __DIR__ . '/packages/' . $name;
-	} elseif (strstr($name, '123WeppsAdmin\\')) {
-		//     	$name = str_replace('\\', '/', $name);
-		//     	$name = str_replace('/Request', '/', $name);
-		//     	$name = substr($name, 0, strrpos($name, "PPS")) . '.php';
-		//         require_once __DIR__ . '/packages/' . $name;
 	}  elseif (strstr($name, 'Wepps')) {
 		$name = str_replace('\\', '/', $name);
 		$name = substr($name, 0, strrpos($name, "Wepps")) . '.php';
