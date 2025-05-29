ad1b1e688e6897a7d223e174c41d06d5f90a54a3
Aleksei Petrov <mail@petroffs.com>
2025-05-29 18:11:06 +0300

    fix: carousel resize off

--- packages/WeppsExtensions/Template/Carousel/Carousel.js
+++ packages/WeppsExtensions/Template/Carousel/Carousel.js
@@ -9,10 +9,10 @@ $(document).ready(function(){
 	};
 	var carousel = ($(window).width()>480) ? '.carousel' : '.carousel-mobile';
 	$(carousel).slick(slickOptions).trigger('init');
-	$(window).resize(function() {
+	/* $(window).resize(function() {
 		carousel = ($(window).width()>480) ? '.carousel' : '.carousel-mobile';
 		$(carousel).slick('unslick');
-	});
+	}); */
 	if ($(carousel).hasClass('slick-initialized')) {
 		$('.carousel-wrapper').css('opacity','1');
 	}
