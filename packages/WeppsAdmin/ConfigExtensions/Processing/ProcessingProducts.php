6b3988bb84b568e1617c8aafa1a2b5ed83d5172a
Aleksei Petrov <mail@petroffs.com>
2025-07-28 10:45:15 +0300

    flow: Генерация Вариаций с количеством

--- packages/WeppsAdmin/ConfigExtensions/Processing/ProcessingProducts.php
+++ packages/WeppsAdmin/ConfigExtensions/Processing/ProcessingProducts.php
@@ -56,11 +56,11 @@ class ProcessingProductsWepps
 		$res = ConnectWepps::$instance->query($sql);
 		$sql = "select Id,Name,NavigatorId from Products";
 		$res = ConnectWepps::$instance->fetch($sql);
-		$sth = ConnectWepps::$db->prepare("update Products set Variations = ? where Id = ?");
+		$sth = ConnectWepps::$db->prepare("update Products set Variations = ?,Quantity = ? where Id = ?");
 		foreach ($res as $value) {
 			#$alias = TextTransformsWepps::translit($value['Name'], 2) . '-' . $value['Id'];
 			$variations = self::_generateProductVariations($value);
-			$sth->execute([$variations, $value['Id']]);
+			$sth->execute([$variations['variations'],$variations['quantitiy'], $value['Id']]);
 		}
 	}
 	/**
@@ -68,7 +68,7 @@ class ProcessingProductsWepps
 	 * 
 	 * @return array Массив вариаций в формате ['color' => string, 'size' => int, ...]
 	 */
-	private function _generateProductVariations(array $product): string
+	private function _generateProductVariations(array $product): array
 	{
 		// Доступные цвета
 		$availableColors = ['Красный', 'Синий', 'Зеленый', 'Черный', 'Белый', 'Желтый', 'Фиолетовый', 'Розовый'];
@@ -87,21 +87,28 @@ class ProcessingProductsWepps
 		// Генерируем все комбинации цветов и размеров
 		##$variations = [];
 		$variations2 = '';
+		$quantitiyAmount = 0;
 		foreach ($selectedColors as $color) {
 			$selectedSizes = array_slice($sizes, 0, rand(2, 3));
 			sort($selectedSizes);
 			foreach ($selectedSizes as $size) {
 				$sku = 'S'.$product['Id'].'-' . mb_strtoupper(mb_substr($color, 0, 3)) . '-' . $size;
+				$quantitiy = rand(4,80);
 				$variations[] = [
 					'color' => $color,
 					'size' => $size,
 					'sku' => $sku,
+					'quantity' => $quantitiy
 				];
-				$variations2 .= "{$color}:::{$size}:::{$sku}\n";
+				$variations2 .= "{$color}:::{$size}:::{$sku}:::{$quantitiy}\n";
+				$quantitiyAmount += $quantitiy;
 			}
 		}
 		$variations2 = trim($variations2);
-		return $variations2;
+		return [
+			'variations' => $variations2,
+			'quantitiy' => $quantitiyAmount
+		];
 	}
 	public function setProductsVariations(array $element) {
 		$fn = function(int $id,array $value) : string {
@@ -131,22 +138,27 @@ class ProcessingProductsWepps
 		$row = [
 				'Name' => '',
 				'DisplayOff' => 0,
+				'Priority' => 0,
 				'ProductsId' => '',
 				'Field1' => '',
 				'Field2' => '',
-				'Field3' => ''
+				'Field3' => '',
+				'Field4' => ''
 			];
 		$prepare = ConnectWepps::$instance->prepare($row);
 		$stmt = ConnectWepps::$db->prepare("update ProductsVariants set {$prepare['update']} where Alias=:Alias");
+		$i=1;
 		foreach ($data as $value) {
 			$alias = $fn($element['Id'],$value);
 			$stmt->execute($row = [
 				'Name' => $value[2]??trim($element['Id'].'-'.@$value[0].'-'.@$value[1],'-'),
 				'DisplayOff' => 0,
+				'Priority' => $i++,
 				'ProductsId' => $element['Id'],
 				'Field1' => @$value[0],
 				'Field2' => @$value[1],
 				'Field3' => @$value[2],
+				'Field4' => @$value[3],
 				'Alias' => $alias
 			]);
 		}
