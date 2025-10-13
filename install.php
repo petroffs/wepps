2250d782a393dc00ce2c2353b9cba3071390cd7f
Aleksei Petrov <mail@petroffs.com>
2025-10-13 19:21:03 +0300

    feat(install): добавление функции смены пароля администратора
    - Добавлена функция changePassword для смены логина и пароля администратора
    - Реализован скрытый ввод пароля с помощью метода getHiddenInput
    - Добавлена проверка формата email для логина
    - Добавлена проверка длины и совпадения паролей
    - Добавлено обновление данных администратора в базе данных
    - Добавлен вызов функции changePassword в основном коде

--- install.php
+++ install.php
@@ -26,7 +26,6 @@ if (empty($projectSettings['DB']['host']) || empty($projectSettings['DB']['port'
 require_once 'configloader.php';
 
 use WeppsCore\Connect;
-use WeppsCore\Utils;
 use WeppsCore\Cli;
 
 /**
@@ -82,6 +81,68 @@ class Install
 		$this->cli->success('База данных заполнена.');
 	}
 
+	public function changePassword(): void
+	{
+		//$this->cli->br();
+		while (true) {
+			$this->cli->warning("Введите логин администратора (email): ");
+			$login = trim(strtolower(fgets(STDIN)));
+			if (!filter_var($login, FILTER_VALIDATE_EMAIL)) {
+				$this->cli->error("Неверный формат email");
+				continue;
+			}
+			break;
+		}
+
+		while (true) {
+			$this->cli->warning("Введите новый пароль администратора (минимум 8 символов): ");
+			$password = $this->getHiddenInput();
+			//$this->cli->br();
+			if (empty($password)) {
+				$this->cli->error("Пароль не может быть пустым.");
+				continue;
+			}
+			if (strlen($password) < 8) {
+				$this->cli->error("Пароль должен содержать не менее 8 символов. Введено: " . strlen($password));
+				continue;
+			}
+			$this->cli->warning("Введите пароль повторно для подтверждения: ");
+			$passwordConfirm = $this->getHiddenInput();
+			//$this->cli->br();
+			if (empty($passwordConfirm)) {
+				$this->cli->error("Подтверждение пароля не может быть пустым.");
+				continue;
+			}
+			if ($password !== $passwordConfirm) {
+				$this->cli->error("Пароли не совпадают. $password & $passwordConfirm");
+				continue;
+			}
+			break;
+		}
+		$row = [
+			'Login' => $login,
+			'Password' => password_hash($password,PASSWORD_BCRYPT),
+			'Email' => $login,
+			'Phone' => '',
+			'CreateDate' => date('Y-m-d H:i:s'),
+			'JCart' => '',
+			'JFav' => '',
+			'JData' => ''
+		];
+		$prepare = Connect::$instance->prepare($row);
+		$sql = "update s_Users set {$prepare['update']} where Id = 1";
+		Connect::$instance->fetch($sql,$prepare['row']);
+		$this->cli->success("Логин и пароль администратора успешно обновлены");
+	}
+
+	private function getHiddenInput(): string
+	{
+		system('stty -echo');
+		$password = trim(fgets(STDIN));
+		system('stty echo');
+		return $password;
+	}
+
 	/**
 	 * Отображение финального сообщения после завершения установки.
 	 */
@@ -93,11 +154,11 @@ class Install
 			$this->cli->warning("Скопируйте файл {$this->config['cnf']} за пределы корневой директории (../config.cnf)");
 			$this->cli->warning('Укажите путь к этому файлу в настройках config.php (DB->cnf)');
 		}
-
 		$this->cli->warning('Удалите файл install.php.');
 	}
 }
 
 $obj = new Install();
 $obj->restoreDB();
+$obj->changePassword();
 $obj->displayFinalMessage();
