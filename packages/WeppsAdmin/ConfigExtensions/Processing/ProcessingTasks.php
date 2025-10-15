1b8b4993522776d29ca17a5ac39da0fc55be32f8
Aleksei Petrov <mail@petroffs.com>
2025-10-16 01:14:14 +0300

    refactor(ProcessingTasks.php): Улучшение метода removeFiles и добавление метода findFiles

--- packages/WeppsAdmin/ConfigExtensions/Processing/ProcessingTasks.php
+++ packages/WeppsAdmin/ConfigExtensions/Processing/ProcessingTasks.php
@@ -1,14 +1,20 @@
 <?php
 namespace WeppsAdmin\ConfigExtensions\Processing;
 
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use WeppsCore\Connect;
+use WeppsCore\Utils;
 
-class ProcessingTasks {
+class ProcessingTasks
+{
 	private $root;
-	public function __construct() {
+	public function __construct()
+	{
 		$this->root = Connect::$projectDev['root'];
 	}
-	public function removeFiles() {
+	public function removeFiles()
+	{
 		/*
 		 * Получить список всех файлов в папке /pic/,/files/lists/
 		 * Составить список для дальнеших действий
@@ -18,19 +24,27 @@ class ProcessingTasks {
 		 * Удалить все файлы, которые попадут в этот список
 		 * Удалить все что в /Addons/Forms/uploads, кроме .htaccess
 		 */
-		
+
 		/*
 		 * Данные в папках
 		 */
-		$output = [];
-		exec("find {$this->root}/files/lists/ {$this->root}/pic/ -name '*.*' ! -name '*.htaccess'",$output);
-		$arr1 = [];
-		foreach ($output as $value) {
+
+
+		$dirs = [
+			$this->root . '/files/lists/',
+			$this->root . '/pic/'
+		];
+		$files = self::findFiles($dirs);
+		// Utils::debug($files,21);
+		// $output = [];
+		// exec("find {$this->root}/files/lists/ {$this->root}/pic/ -name '*.*' ! -name '*.htaccess' 2>&1", $output);
+		// $arr1 = [];
+		// Utils::debug($output, 21);
+		foreach ($files as $value) {
 			$str = substr($value, strpos($value, "/files/"));
 			$arr1[$str][] = $value;
 		}
 		$arr1keys = array_keys($arr1);
-		
 		/*
 		 * Данные в базе
 		 */
@@ -40,26 +54,45 @@ class ProcessingTasks {
 		foreach ($res as $value) {
 			$arr2[] = $value['FileUrl'];
 		}
-		
+
 		/*
 		 * Расхождение
 		 */
 		$diff = array_diff($arr1keys, $arr2);
-		
+
 		/*
 		 * Удаление файлов, не записанных в s_Files
 		 */
 		$i = 0;
-		if (count ( $diff ) != 0) {
-			foreach ( $diff as $value ) {
-				if (isset ( $arr1 [$value] )) {
-					foreach ( $arr1 [$value] as $filename ) {
-						unlink ( $filename );
+		if (count($diff) != 0) {
+			foreach ($diff as $value) {
+				if (isset($arr1[$value])) {
+					foreach ($arr1[$value] as $filename) {
+						unlink($filename);
 						$i++;
 					}
 				}
 			}
 		}
 		echo "\n$i files deleted\n";
+	}
+	private function findFiles($dirs, $exclude = ['.htaccess'])
+	{
+		$files = [];
+		foreach ((array) $dirs as $dir) {
+			if (!is_dir($dir))
+				continue;
+
+			$iterator = new RecursiveIteratorIterator(
+				new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS)
+			);
+
+			foreach ($iterator as $file) {
+				if ($file->isFile() && !in_array($file->getFilename(), $exclude)) {
+					$files[] = str_replace("\\", "/", $file->getPathname());
+				}
+			}
+		}
+		return $files;
 	}
 }
