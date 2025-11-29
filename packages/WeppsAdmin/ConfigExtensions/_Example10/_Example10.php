3982f16f1d8963375e53c335bf5102e8f2a16ab9
Aleksei Petrov <mail@petroffs.com>
2025-11-29 12:53:55 +0300

    refactor: улучшена читаемость кода в классе _Example10

--- packages/WeppsAdmin/ConfigExtensions/_Example10/_Example10.php
+++ packages/WeppsAdmin/ConfigExtensions/_Example10/_Example10.php
@@ -6,26 +6,32 @@ use WeppsCore\Smarty;
 use WeppsCore\TemplateHeaders;
 use WeppsCore\Exception;
 
-class _Example10 extends Request {
-	private $title;
-	public function request($action="") {
+class _Example10 extends Request
+{
+	public $title;
+	public function request($action = "")
+	{
 		$smarty = Smarty::getSmarty();
 		$this->tpl = '_Example10.tpl';
 		$this->title = $this->get['ext']['Name'];
-		$this->way = [[	'Url'=>"/_wepps/extensions/{$this->get['ext']['Alias']}/",
-						'Name'=>$this->title]];
-		$smarty->assign('test1','test1');
-		$smarty->assign('url','/packages/WeppsAdmin/ConfigExtensions/_Example10/Request.php');
+		$this->way = [
+			[
+				'Url' => "/_wepps/extensions/{$this->get['ext']['Alias']}/",
+				'Name' => $this->title
+			]
+		];
+		$smarty->assign('test1', 'test1');
+		$smarty->assign('url', '/packages/WeppsAdmin/ConfigExtensions/_Example10/Request.php');
 		$headers = new TemplateHeaders();
-		$headers->js ("/packages/WeppsAdmin/ConfigExtensions/_Example10/_Example10.{$headers::$rand}.js");
-		$headers->css ("/packages/WeppsAdmin/ConfigExtensions/_Example10/_Example10.{$headers::$rand}.css");
+		$headers->js("/packages/WeppsAdmin/ConfigExtensions/_Example10/_Example10.{$headers::$rand}.js");
+		$headers->css("/packages/WeppsAdmin/ConfigExtensions/_Example10/_Example10.{$headers::$rand}.css");
 		switch ($action) {
 			case 'examplelink':
 				$this->title = "Тест";
 				$this->tpl = '_Example10Test.tpl';
 				break;
 			default:
-				if ($action!="") {
+				if ($action != "") {
 					Exception::error404();
 				}
 				break;
